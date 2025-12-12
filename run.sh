5987d546892e6d2dbf1cfc649fd8775ac67ca8a5
Daniel Bayu A.R.S <daniel.bayu@gmail.com>
2025-12-12 13:29:36 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -31,8 +31,8 @@ fi
 # Update docker-compose configuration
 if [[ "$OSTYPE" == "darwin"* ]]; then
   # macOS sed syntax
-  sed -i '' 's/30019/'$PORT'/g' $DESTINATION/docker-compose.yml
-  sed -i '' 's/40019/'$CHAT'/g' $DESTINATION/docker-compose.yml
+  sed -i '' 's/10019/'$PORT'/g' $DESTINATION/docker-compose.yml
+  sed -i '' 's/20019/'$CHAT'/g' $DESTINATION/docker-compose.yml
 else
   # Linux sed syntax
   sed -i 's/30019/'$PORT'/g' $DESTINATION/docker-compose.yml
